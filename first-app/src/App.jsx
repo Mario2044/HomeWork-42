d10cd7f5898c0fd2c9c2e25715cb3032b4309592
Mario2044 <116796406+Mario2044@users.noreply.github.com>
2023-06-04 08:56:20 +0300

    Сompleted

--- first-app/src/App.jsx
+++ first-app/src/App.jsx
@@ -2,51 +2,53 @@ import React, { useState, useEffect } from "react";
 import './App.css';
 import Menu from './menu/menu';
 
-function Weather () {
-  const [city, setCity] = useState("");
-  const [temp, setTemp] = useState("");
-  const [pressure, setPressure] = useState("");
-  const [description, setDescription] = useState("");
-  const [humidity, setHumidity] = useState("");
-  const [speed, setSpeed] = useState("");
-  const [deg, setDeg] = useState("");
-  const [icon, setIcon] = useState("");
+function Weather() {
+  const [weatherData, setWeatherData] = useState({
+    city: "",
+    temp: "",
+    pressure: "",
+    description: "",
+    humidity: "",
+    speed: "",
+    deg: "",
+    icon: ""
+  });
 
-  useEffect (() => {
+  useEffect(() => {
     const url = "http://api.openweathermap.org/data/2.5/weather?q=LVIV&units=metric&APPID=5d066958a60d315387d9492393935c19";
 
     fetch(url)
       .then(response => response.json())
       .then(data => {
-        setCity(data.name);
-        setTemp(data.main.temp);
-        setPressure(data.main.pressure);
-        setDescription(data.weather[0].description);
-        setHumidity(data.main.humidity);
-        setSpeed(data.wind.speed);
-        setDeg(data.wind.deg);
-        setIcon(`http://openweathermap.org/img/w/${data.weather[0].icon}.png`);
+        setWeatherData({
+          city: data.name,
+          temp: data.main.temp,
+          pressure: data.main.pressure,
+          description: data.weather[0].description,
+          humidity: data.main.humidity,
+          speed: data.wind.speed,
+          deg: data.wind.deg,
+          icon: `http://openweathermap.org/img/w/${data.weather[0].icon}.png`
+        });
       })
       .catch(error => console.error(error));
   }, []);
 
   return (
-    <div class="wrap">
-      <div>
+    <div className="wrap">
       <Menu />
-      </div>
       <div>
-      <div class="header">Погода у місті {city}</div>
-        <div class="unit">
-          <p>Температура: {temp} °C</p>
-          <p>Тиск: {pressure} гПа</p>
-          <p>Опис: {description}</p>
-          <p>Вологість: {humidity}%</p>
-          <p>Швидкість вітру: {speed} м/с</p>
-          <p>Напрям вітру: {deg}°</p> 
+        <div className="header">Погода у місті {weatherData.city}</div>
+        <div className="unit">
+          <p>Температура: {weatherData.temp} °C</p>
+          <p>Тиск: {weatherData.pressure} гПа</p>
+          <p>Опис: {weatherData.description}</p>
+          <p>Вологість: {weatherData.humidity}%</p>
+          <p>Швидкість вітру: {weatherData.speed} м/с</p>
+          <p>Напрям вітру: {weatherData.deg}°</p>
         </div>
         <div className="img">
-        <img src={icon} alt="icon" />
+          <img src={weatherData.icon} alt="icon" />
         </div>
       </div>
     </div>
--- first-app/src/menu/menu.jsx
+++ first-app/src/menu/menu.jsx
@@ -1,50 +1,37 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './menu.css'; 
 
-class Menu extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isMenuOpen: false,
-    };
-  }
+function Menu() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  handleMenuEnter = () => {
-    this.setState({
-      isMenuOpen: true,
-    });
+  const handleMenuEnter = () => {
+    setIsMenuOpen(true);
   };
 
-  handleMenuLeave = () => {
-    this.setState({
-      isMenuOpen: false,
-    });
+  const handleMenuLeave = () => {
+    setIsMenuOpen(false);
   };
 
-  render() {
-    const { isMenuOpen } = this.state;
-
-    return (
-      <div
-        className={`menu-container ${isMenuOpen ? 'open' : ''}`}
-        onMouseEnter={this.handleMenuEnter}
-        onMouseLeave={this.handleMenuLeave}
-      >
-        <div className="menu-button">
-          <div className="menu-icon">
-            <span>Меню</span>
-          </div>
-        </div>
-        <div className={`menu-content ${isMenuOpen ? 'open' : ''}`}>
-          <p><a href="#">Головна</a></p>
-          <p><a href="#">Погода в інших містах</a></p>
-          <p><a href="#">Новини</a></p>
-          <p><a href="#">Про Нас</a></p>
-          <p><a href="#">Допомога</a></p>
+  return (
+    <div
+      className={`menu-container ${isMenuOpen ? 'open' : ''}`}
+      onMouseEnter={handleMenuEnter}
+      onMouseLeave={handleMenuLeave}
+    >
+      <div className="menu-button">
+        <div className="menu-icon">
+          <span>Меню</span>
         </div>
       </div>
-    );
-  }
+      <div className={`menu-content ${isMenuOpen ? 'open' : ''}`}>
+        <p><a href="#">Головна</a></p>
+        <p><a href="#">Погода в інших містах</a></p>
+        <p><a href="#">Новини</a></p>
+        <p><a href="#">Про Нас</a></p>
+        <p><a href="#">Допомога</a></p>
+      </div>
+    </div>
+  );
 }
 
-export default Menu;
+export default Menu
